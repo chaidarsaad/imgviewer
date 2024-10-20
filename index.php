a1317da394b515436eebadf4fd54e6b86ebb66c4
chaidarsaad <chaidarsaad55@gmail.com>
2024-10-20 12:29:29 +0700

    update

--- index.php
+++ index.php
@@ -127,52 +127,53 @@ if (isset($_POST['patient_id'])) {
 		</form>
 		<div class="results">
 			<?php if (isset($studies) && !empty($studies)): ?>
-				<h2>Studi yang Ditemukan:</h2>
-				<ul>
-					<?php foreach ($studies as $studyId): ?>
-						<li>
-							<?php
-							$studyUrl = $orthancUrl . '/studies/' . $studyId;
-							$ch = curl_init($studyUrl);
-							curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-							$studyDetails = json_decode(curl_exec($ch), true);
-							curl_close($ch);
+				<h1>Studi yang Ditemukan Pasien ID: <?php echo $patientId ?></h2>
+					<ul>
+						<?php foreach ($studies as $studyId): ?>
+							<li>
+								<h3>Study ID: <?php echo $studyId; ?></h3>
+								<?php
+								$studyUrl = $orthancUrl . '/studies/' . $studyId;
+								$ch = curl_init($studyUrl);
+								curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+								$studyDetails = json_decode(curl_exec($ch), true);
+								curl_close($ch);
 
-							// echo "<pre>";
-							// print_r($studyDetails);
-							// echo "</pre>";
+								// echo "<pre>";
+								// print_r($studyDetails);
+								// echo "</pre>";
 
-							if (!empty($studyDetails['Series'])) {
-								foreach ($studyDetails['Series'] as $seriesId) {
-									$seriesUrl = $orthancUrl . '/series/' . $seriesId;
-									$ch = curl_init($seriesUrl);
-									curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-									$seriesDetails = json_decode(curl_exec($ch), true);
-									curl_close($ch);
-									echo "<pre>";
+								if (!empty($studyDetails['Series'])) {
+									foreach ($studyDetails['Series'] as $seriesId) {
+										$seriesUrl = $orthancUrl . '/series/' . $seriesId;
+										$ch = curl_init($seriesUrl);
+										curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+										$seriesDetails = json_decode(curl_exec($ch), true);
+										curl_close($ch);
+										echo "<pre>";
 
-									echo "<h4>Series ID: $seriesId</h4>";
+										echo "<h4>Series ID: $seriesId</h4>";
 
-									if (!empty($seriesDetails['Instances'])) {
-										foreach ($seriesDetails['Instances'] as $instanceId) {
-											echo "<p>Preview Gambar Instance ID: $instanceId</p>";
-											echo "<img src='" . getImagePreview($orthancUrl, $instanceId) . "' alt='DICOM Preview'>";
+										if (!empty($seriesDetails['Instances'])) {
+											foreach ($seriesDetails['Instances'] as $instanceId) {
+												echo "<p>Preview Gambar Instance ID: $instanceId</p>";
+												echo "<img src='" . getImagePreview($orthancUrl, $instanceId) . "' alt='DICOM Preview'>";
+											}
+										} else {
+											echo "<p>Tidak ada instance ditemukan untuk Series ID ini.</p>";
 										}
-									} else {
-										echo "<p>Tidak ada instance ditemukan untuk Series ID ini.</p>";
+										echo "</pre>";
 									}
-									echo "</pre>";
+								} else {
+									echo "<p>Tidak ada series ditemukan untuk Study ID ini.</p>";
 								}
-							} else {
-								echo "<p>Tidak ada series ditemukan untuk Study ID ini.</p>";
-							}
-							?>
-						</li>
-					<?php endforeach; ?>
-				</ul>
-			<?php elseif (isset($patientId)): ?>
-				<p>Tidak ada studi ditemukan untuk Patient ID: <?php echo htmlspecialchars($patientId); ?></p>
-			<?php endif; ?>
+								?>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				<?php elseif (isset($patientId)): ?>
+					<p>Tidak ada studi ditemukan untuk Patient ID: <?php echo htmlspecialchars($patientId); ?></p>
+				<?php endif; ?>
 		</div>
 	</div>
 
